ae2f3d52535e9e3d4a6ea0178c6a8da182d6e9c4
peter enejo <petstorm101@gmail.com>
2022-06-08 06:53:05 +0100

    commit tree 9703292d6751237dc58cc8d17d251cebe8cbb9fb

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+  if(feet < 400){
+    return 'This one is on me!'
+  }   else if(feet === 400){
+        return 'This one is on me!'
+  } 
 
-function ternaryCheckCity(){
+     else if (feet > 2000 && feet <= 2500 ){
+       return 'I will gladly take your thirty bucks.'
+  }  else if (feet > 2500){
+       return 'No can do.'
+}
+}  
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+   return city === 'NYC'?'Ok, sounds good.' : "No go."
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+// Write your code here!
 }
